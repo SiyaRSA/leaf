e98a4e320bea6ca757b32f7d1d82283646e42c31
Mzwandile <ms.tshabalala@mail.com>
2026-05-07 12:44:10 +0200

    VertexBufferLayout for VecG

--- src/util/point.rs
+++ src/util/point.rs
@@ -1,6 +1,7 @@
 //point.rs
 use crate::util::vector::Vec2;
 use bytemuck::{Pod, Zeroable};
+use wgpu::VertexBufferLayout;
 
 ///Point is used to represent a vetex along a 2D [`Path`]
 #[repr(C)]
@@ -18,16 +19,21 @@ impl Point {
     pub fn vec2(&self) -> Vec2 {
        Vec2 { x: self.x, y: self.y }
     }
-
-    pub fn desc() -> wgpu::VertexBufferLayout<'static> {
-        wgpu::VertexBufferLayout { 
-            array_stride: std::mem::size_of::<Point>() as wgpu::BufferAddress,
+    
+    pub fn dec() -> VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<f32>() as wgpu::BufferAddress * 6,
             step_mode: wgpu::VertexStepMode::Vertex,
             attributes: &[
                 wgpu::VertexAttribute {
                     offset: 0,
                     shader_location: 0,
                     format: wgpu::VertexFormat::Float32x2,
+                },
+                wgpu::VertexAttribute {
+                    offset: 8,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x4
                 },
             ],
         }
